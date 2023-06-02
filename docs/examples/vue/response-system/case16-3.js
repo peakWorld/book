20177d2c350ca92b52b07e5f1d348ab8cf40ce78
lyf <823088131@qq.com>
2023-06-02 17:24:12 +0800

    week 5

--- docs/examples/vue/response-system/case16-3.js
+++ docs/examples/vue/response-system/case16-3.js
@@ -10,6 +10,17 @@ function parse(str) {
   const context = {
     source: str,
     mode: TextModes.DATA,
+    // 用来消费指定数量的字符, 接收一个数字参数
+    advanceBy(num) {
+      context.source = context.source.slice(num);
+    },
+    // 匹配空白字符
+    advanceSpaces() {
+      const match = /^[\t\r\n\f\s]+/.exec(context.source);
+      if (match) {
+        context.advanceBy(match[0].length);
+      }
+    },
   };
   const nodes = parseChildren(context, []);
   return { type: 'Root', children: nodes };
@@ -21,33 +32,25 @@ function parseChildren(context, ancestors) {
 
   while (!isEnd(context, ancestors)) {
     let node;
-    // 只有DATA 和 RCDATA模式才支持插值节点的解析
     if (mode === TextModes.DATA || mode === TextModes.RCDATA) {
-      // 只有DATA模式才支持标签节点的解析
       if (mode === TextModes.DATA && source[0] === '<') {
         if (source[1] === '!') {
           if (source.startWith('<!--')) {
-            // 注释
             node = parseComment(context);
           } else if (source.startWith('<![CDATA[')) {
             node = parseCDATA(context, ancestors);
           }
         } else if (source[1] === '/') {
-          // 结束标签
-          // 状态机遭遇了闭合标签, 抛出错误
           console.error('无效的结束标签');
           continue;
         } else if (/[a-z]/i.test(source[1])) {
-          // 标签
           node = parseElement(context, ancestors);
         }
       } else if (source.startWith('{{')) {
-        // 解析插值
         node = parseInterpolation(context);
       }
     }
 
-    // node不存在, 说明处于其他模式(非DATA、非RCDATA)
     if (!node) {
       node = parseText(context);
     }
@@ -58,10 +61,8 @@ function parseChildren(context, ancestors) {
   return nodes;
 }
 
-// 解释二
 function isEnd(context, ancestors) {
   if (!context.source) return true;
-  // 与栈中所有节点做比较
   for (let i = ancestors.length - 1; i >= 0; --i) {
     if (context.source.startWith(`</${parent.tag}`)) {
       return true;
@@ -70,12 +71,19 @@ function isEnd(context, ancestors) {
 }
 
 function parseElement(context, ancestors) {
-  // 解析开始标签
   const element = parseTag(context);
   if (element.isSelfClosing) return element;
 
+  // 切换正确的文本模式
+  if (element.tag === 'textarea' || element.tag === 'title') {
+    context.mode = TextModes.RCDATA;
+  } else if (/style|xmp|iframe|noembed|noframes|noscript/.test(element.tag)) {
+    context.mode = TextModes.RAWTEXT;
+  } else {
+    context.mode = TextModes.DATA;
+  }
+
   ancestors.push(element);
-  // 递归调用 parseChildren 函数进行 标签字节点的解析
   element.children = parseChildren(context, ancestors);
   ancestors.pop();
 
@@ -85,4 +93,29 @@ function parseElement(context, ancestors) {
     console.error(`${element.tag} 标签缺少闭合标签`);
   }
   return element;
+}
+
+function parseTag(context, type = 'start') {
+  const { advanceBy, advanceSpaces } = context;
+
+  // 利用正则 匹配开始、结束标签
+  const match = (type = 'start'
+    ? /^<([a-z][^\t\r\n\f\s]*)/i.exec(context.source) // 开始标签
+    : /^<\/([a-z][^\t\r\n\f\s]*)/i.exec(context.source)); // 结束标签
+
+  const tag = match[1]; // 正则表达式的第一个捕获组的值就是标签名 div
+  advanceBy(match[0].length); // 消费正则表达式匹配的全部内容 <div
+  advanceSpaces(); // 消费标签中无用的空白字符
+
+  // 如果字符以 '/>' 开头, 则表明是一个自闭合标签
+  const isSelfClosing = context.source.startWith('/>');
+  advanceBy(isSelfClosing ? 2 : 1);
+
+  return {
+    type: 'Element',
+    tag,
+    props: [],
+    children: [],
+    isSelfClosing,
+  };
 }
